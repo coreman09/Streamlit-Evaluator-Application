6224a90f1eb3232bb1c6a1b37326aa719a671243
coreman09 <coreymgray@yahoo.com>
2026-01-07 12:23:12 -0500

    Update evaluator_optomization_app.py

--- evaluator_optomization_app.py
+++ evaluator_optomization_app.py
@@ -36,7 +36,7 @@ mileage_df['Status'] = mileage_df['Evaluator'].apply(
 
 # Ensure numeric columns
 mileage_df['Round-Trip Miles'] = pd.to_numeric(mileage_df['Round-Trip Miles'], errors='coerce')
-mileage_df['cost ($)'] = pd.to_numeric(mileage_df['cost ($)'], errors='coerce')
+mileage_df['2026 Cost'] = pd.to_numeric(mileage_df['2026 Cost'], errors='coerce')
 
 # Add Per Diem (contractors only)
 mileage_df['Per Diem'] = mileage_df.apply(
@@ -147,4 +147,5 @@ st.download_button(
     mime="text/csv"
 
 )
+
 
